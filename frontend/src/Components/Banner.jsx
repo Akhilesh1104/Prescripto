ea2e166aa94bc5c48fb7d8d639160015d97f8d98
Akhilesh1104 <akhileshgupta2404@gmail.com>
2025-02-13 11:36:27 +0530

    Update

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -19,14 +19,14 @@ const Banner = () => {
             <p className="mt-4">With 100+ Trusted Doctors</p>
         </div>
         {
-          !token && <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all">Create Account</button>
+          !token && <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all duration-500">Create Account</button>
         }
       </div>
 
       {/*-------------- Right Side--------- */}
 
       <div className="hidden md:block md:w-1/2 lg:w-[370px] relative ">
-        <img className="w-full absolute bottom-0 right-0 max-w-md" src='https://i.imghippo.com/files/Nxa1885lc.png' alt="" />
+        <img className="w-full absolute bottom-0 right-0 max-w-md" src={assets.appointment_img} alt="" />
       </div>
 
     </div>
